62c40409a2c8d5af495261166cf882f5bc9c6ab9
Kp707241121 <110337363+Kp707241121@users.noreply.github.com>
2025-06-23 17:16:48 -0400

    Update 1_🏆_Standings.py

--- pages/1_🏆_Standings.py
+++ pages/1_🏆_Standings.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from leagueManager import LeagueManager
 from manualmatchup import Scores# ✅ Use your custom Matchup subclass
-
+from espn_api.baseball import Team
 # Inject your patched Scores logic into ESPN League
 from espn_api.baseball import League
 League._matchup_class = Scores  # ✅ Force all matchups to use your patched class
