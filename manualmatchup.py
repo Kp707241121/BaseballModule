d098c42b6d8b9da096612c1f9f2b387fbf631e5e
Kp707241121 <110337363+Kp707241121@users.noreply.github.com>
2025-06-23 15:48:49 -0400

    Update manualmatchup.py

--- manualmatchup.py
+++ manualmatchup.py
@@ -1,5 +1,6 @@
-from espn_api.baseball import Matchup as matchup
-class Scores(matchup):
+from espn_api.baseball import Matchup as Basematchup
+
+class Scores(Basematchup):
     '''Creates Matchup instance'''
     def __init__(self, data, week=None):
         self.week = week
@@ -8,29 +9,32 @@ class Scores(matchup):
         self._fetch_matchup_info(data)
 
     def __repr__(self):
-        # TODO: use final score when that's available?
-        # writing this too early to see if data['home']['totalPoints'] is final score
-        # it might also be used for points leagues instead of category leagues
         if not self.away_team_live_score:
             return 'Matchup(%s, %s)' % (self.home_team, self.away_team)
         else:
-            return 'Matchup(%s %s - %s %s)' % (self.home_team,
-                                               str(round(self.home_team_live_score, 1)),
-                                               str(round(self.away_team_live_score, 1)),
-                                               self.away_team)
+            return 'Matchup(%s %s - %s %s)' % (
+                self.home_team,
+                str(round(self.home_team_live_score, 1)),
+                str(round(self.away_team_live_score, 1)),
+                self.away_team
+            )
 
     def _fetch_matchup_info(self, data):
-        '''Fetch info for matchup'''
         self.home_team = data['home']['teamId']
         self.home_final_score = data['home']['totalPoints']
         self.away_team = data['away']['teamId']
         self.away_final_score = data['away']['totalPoints']
         self.winner = data['winner']
 
-        # if stats are available
-        if 'cumulativeScore' in data['home'].keys() and data['home']['cumulativeScore']['scoreByStat']:
+        # ✅ Assign the week to matchup_period
+        self.matchup_period = self.week
 
-            self.home_team_live_score = (data['home']['cumulativeScore']['wins'] +
-                                         data['home']['cumulativeScore']['ties']/2)
-            self.away_team_live_score = (data['away']['cumulativeScore']['wins'] +
-                                         data['away']['cumulativeScore']['ties']/2)
+        if 'cumulativeScore' in data['home'] and data['home']['cumulativeScore']['scoreByStat']:
+            self.home_team_live_score = (
+                data['home']['cumulativeScore']['wins'] +
+                data['home']['cumulativeScore']['ties'] / 2
+            )
+            self.away_team_live_score = (
+                data['away']['cumulativeScore']['wins'] +
+                data['away']['cumulativeScore']['ties'] / 2
+            )
