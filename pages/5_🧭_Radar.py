61df53871093c17355fc3e685f39f70d9d5fd9d3
Kp707241121 <110337363+Kp707241121@users.noreply.github.com>
2025-06-24 06:03:08 -0400

    Update 5_🧭_Radar.py

--- pages/5_🧭_Radar.py
+++ pages/5_🧭_Radar.py
@@ -4,13 +4,7 @@ import streamlit as st
 import json
 import pandas as pd
 import plotly.express as px
-from login import login  # or adjust import path if needed
 
-# --- Restrict Page Access ---
-if "role" not in st.session_state or st.session_state.role not in ["User", "Admin"]:
-    st.warning("You must log in to access this page.")
-    login()
-    st.stop()
 
 STAT_ORDER = ['R', 'HR', 'RBI', 'OBP', 'SB', 'K', 'W', 'SV', 'ERA', 'WHIP']
 INVERT_STATS = {'ERA', 'WHIP', 'OBP'} 
