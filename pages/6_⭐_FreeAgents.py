6b738970d809b31d7d77d76dab01f819efa4f0d4
Kp707241121 <110337363+Kp707241121@users.noreply.github.com>
2025-07-10 01:28:57 -0400

    Your message

--- pages/6_⭐_FreeAgents.py
+++ pages/6_⭐_FreeAgents.py
@@ -8,10 +8,10 @@ from leagueManager import LeagueManager
 from free_agents import FreeAgents
 
 # --- Restrict Page Access ---
-"""if "role" not in st.session_state or st.session_state.role not in ["User", "Admin"]:
+if "role" not in st.session_state or st.session_state.role not in ["User", "Admin"]:
     st.warning("You must log in to access this page.")
     login()
-    st.stop()"""
+    st.stop()
     
 PITCHING_STATS = ['K', 'W', 'SV', 'ERA', 'WHIP']
 HITTING_STATS = ['R', 'HR', 'RBI', 'OBP', 'SB']
